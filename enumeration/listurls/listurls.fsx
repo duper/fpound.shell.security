067940406b17d66089e60314cadafa15de6a7eca
David Klein <david.r.klein@gmail.com>
2011-04-02 12:59:04 +1100

    listurlsbeta

---
+++ enumeration/listurls/listurls.fsx
@@ -0,0 +1,38 @@
+// Replacement for /pentest/enumeration/list-urls
+// TODO: make fsx friendly, take a file as list, hostname resolver, crawler, https
+
+#light
+open System.Net
+open System.Text.RegularExpressions
+open Microsoft.FSharp.Control.WebExtensions
+
+let urlList = ["1","http://google.com"]
+
+let extractLinksAsync html =
+    async {
+        let linkCount = Regex.Matches(html, @"http://\S+")
+        linkCount |> Seq.cast |> Seq.iter (fun s -> printfn "\x22%A" s)
+        return linkCount
+    }
+           
+    
+let fetchAsync(name, url:string) =
+    async {
+        try
+            let uri = new System.Uri(url)
+            let webClient = new WebClient()
+            let! html = webClient.AsyncDownloadString(uri)
+            let! links = extractLinksAsync html
+            printf "%s : %d" url links.Count
+
+        with | ex -> printfn "Finished, Total links: %s" (ex.Message)
+    }
+
+
+let runAll =
+    urlList
+        |> Seq.map fetchAsync
+        |> Async.Parallel 
+        |> Async.RunSynchronously
+        |> ignore
+
